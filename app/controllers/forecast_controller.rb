979939510d2f6857c5e93640bf1f0b4b79fab0f5
Belizario Rocha <rocha.belizario@chicagobooth.edu>
2017-07-11 21:19:41 +0000

    2 of 3 complete

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,20 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    url = "https://api.darksky.net/forecast/c4c259ad626dd984cfbe8b54a62d6924/#{@lat},#{@lng}"
+    # url = "https://api.darksky.net/forecast/c4c259ad626dd984cfbe8b54a62d6924/41.78,-87.59"
+    
+    parsed_data = JSON.parse(open(url).read)
 
+    @current_temperature = parsed_data.dig("currently", "temperature")
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = parsed_data.dig("currently", "summary")
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data.dig("minutely", "summary")
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data.dig("hourly", "summary")
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data.dig("daily", "summary")
 
     render("forecast/coords_to_weather.html.erb")
   end
